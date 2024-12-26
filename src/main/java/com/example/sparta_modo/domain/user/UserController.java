863562c75d8f26b470b29b846d78e598e5023bc0
Seonu-Jeong <sw.jeong97@gmail.com>
2024-12-26 20:18:07 +0900

    update : 회원가입 API 추가

--- src/main/java/com/example/sparta_modo/domain/user/UserController.java
+++ src/main/java/com/example/sparta_modo/domain/user/UserController.java
@@ -1,13 +1,31 @@
 package com.example.sparta_modo.domain.user;
 
+import com.example.sparta_modo.domain.user.dto.SignUpDto;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/users")
 @RequiredArgsConstructor
+@Slf4j
 public class UserController {
 
+	private final UserService userService;
 
+	@PostMapping
+	public ResponseEntity<SignUpDto.Response> signUp(
+		@Valid @RequestBody SignUpDto.Request requestDto
+	) {
+
+		SignUpDto.Response response = userService.signUp(requestDto);
+
+		return ResponseEntity.status(HttpStatus.CREATED).body(response);
+	}
 }
---
+++ src/main/java/com/example/sparta_modo/domain/user/dto/SignUpDto.java
@@ -0,0 +1,57 @@
+package com.example.sparta_modo.domain.user.dto;
+
+import com.example.sparta_modo.global.annotation.ValidEnum;
+import com.example.sparta_modo.global.entity.User;
+import com.example.sparta_modo.global.entity.enums.Auth;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import lombok.Getter;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+public class SignUpDto {
+
+	@Getter
+	public static class Request {
+
+		@NotBlank(message = "email 입력은 필수입니다")
+		@Email(message = "올바른 email 형식이 아닙니다")
+		private String email;
+
+		@NotBlank(message = "password 입력은 필수입니다")
+		@Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$", message = "비밀번호는 최소 8글자 이상이며, 영문, 숫자, 특수문자를 1개씩 포함해야합니다.")
+		private String password;
+
+		@NotBlank(message = "nickname은 빈 값이 허용되지 않습니다")
+		private String nickname;
+
+		@ValidEnum(enumClass = Auth.class, message = "유효하지 않은 auth입니다.")
+		private String auth;
+
+		public User toEntity() {
+			return User.builder()
+				.email(email)
+				.password(password)
+				.nickname(nickname)
+				.auth(Auth.of(auth))
+				.build();
+		}
+
+		public void encodePassword(PasswordEncoder encoder){
+				password = encoder.encode(password);
+		}
+	}
+
+	@Getter
+	public static class Response {
+		private Long userId;
+
+		private String auth;
+
+		public Response(Long userId, String auth) {
+			this.userId = userId;
+			this.auth = auth;
+		}
+	}
+
+}
