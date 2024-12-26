ea334c20fb8ce04ddf17269fbdba8474d3a10902
Seonu-Jeong <sw.jeong97@gmail.com>
2024-12-26 20:14:13 +0900

    update : ALREADY_EXIST 에러 코드 추가

--- src/main/java/com/example/sparta_modo/global/exception/errorcode/ErrorCode.java
+++ src/main/java/com/example/sparta_modo/global/exception/errorcode/ErrorCode.java
@@ -8,9 +8,6 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum ErrorCode {
 
-    // 400 BAD REQUEST
-    //BAD_REQUEST(HttpStatus.BAD_REQUEST, "잘못된 요청입니다."),
-
     // 401 UNAUTHORIZED(User, Workspace, Board, List, Card, Comment 모든 Unauthorized)
     UNAUTHORIZED_ACCESS(HttpStatus.UNAUTHORIZED,"인증에 실패했습니다."),
 
@@ -18,8 +15,9 @@ public enum ErrorCode {
     FORBIDDEN_ACCESS(HttpStatus.FORBIDDEN, "권한이 없는 접근입니다."),
 
     // 404 NOT FOUND (User, Workspace, Board, List, Card, Comment 모든 Not Found)
-    NOT_FOUND_VALUE(HttpStatus.NOT_FOUND, "해당 정보가 없습니다.")
-    ;
+    NOT_FOUND_VALUE(HttpStatus.NOT_FOUND, "해당 정보가 없습니다."),
+
+    ALREADY_EXIST(HttpStatus.BAD_REQUEST, "이미 존재하는 값입니다.");
 
     private final HttpStatus httpStatus;
     private final String detail;
