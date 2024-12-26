e963aaef1234d2a67a9a5f4e1cb9c6bc3fe66630
Seonu-Jeong <sw.jeong97@gmail.com>
2024-12-26 20:13:14 +0900

    update : of( ) 메소드 추가

--- src/main/java/com/example/sparta_modo/global/entity/enums/Auth.java
+++ src/main/java/com/example/sparta_modo/global/entity/enums/Auth.java
@@ -1,6 +1,20 @@
 package com.example.sparta_modo.global.entity.enums;
 
-public enum Auth {
-    USER, ADMIN
+import lombok.Getter;
 
+@Getter
+public enum Auth {
+    USER,
+    ADMIN;
+
+    public static Auth of(String authName) throws IllegalArgumentException {
+        //TODO 리팩토링 고려
+        for (Auth auth : values()) {
+            if (auth.name().equals(authName)) {
+                return auth;
+            }
+        }
+
+        throw new IllegalArgumentException("해당하는 이름의 권한을 찾을 수 없습니다: " + authName);
+    }
 }
