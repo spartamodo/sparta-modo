655ce3938ed796a2444f075cc4606ca37a4a1e52
Seonu-Jeong <sw.jeong97@gmail.com>
2024-12-26 20:39:41 +0900

    fix : @Sl4j 제거

--- src/main/java/com/example/sparta_modo/domain/user/UserController.java
+++ src/main/java/com/example/sparta_modo/domain/user/UserController.java
@@ -3,7 +3,6 @@ package com.example.sparta_modo.domain.user;
 import com.example.sparta_modo.domain.user.dto.SignUpDto;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,7 +13,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/users")
 @RequiredArgsConstructor
-@Slf4j
 public class UserController {
 
 	private final UserService userService;
