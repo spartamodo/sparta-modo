606515e54de84c33886f620091a4b7b24c758da1
Seonu-Jeong <sw.jeong97@gmail.com>
2024-12-27 19:20:16 +0900

    update : 유저 에러 코드 추가

--- src/main/java/com/example/sparta_modo/global/exception/errorcode/UserErrorCode.java
+++ src/main/java/com/example/sparta_modo/global/exception/errorcode/UserErrorCode.java
@@ -8,8 +8,10 @@ import org.springframework.http.HttpStatus;
 @Getter
 @AllArgsConstructor
 public enum UserErrorCode {
-    NOT_FOUND_VALUE(HttpStatus.NOT_FOUND, "해당 정보가 없습니다.")
-    ;
+
+    NOT_MATCH_PASSOWORD(HttpStatus.BAD_REQUEST, "비밀번호가 맞지 안습니다"),
+
+    ALREADY_DELETED_USER(HttpStatus.BAD_REQUEST, "이미 삭제된 유접입니다");
 
     private final HttpStatus httpStatus;
     private final String detail;
