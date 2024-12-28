63d3f58749175e5c51f5da0818479350d32d59c5
kimuky <xogus559@gmail.com>
2024-12-29 04:07:52 +0900

    add: BoardImage 엔티티 추가

--- src/main/java/com/example/sparta_modo/global/entity/BoardImage.java
+++ src/main/java/com/example/sparta_modo/global/entity/BoardImage.java
@@ -1,4 +1,22 @@
 package com.example.sparta_modo.global.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity(name = "board_image")
+@NoArgsConstructor
 public class BoardImage {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @OneToOne
+    @JoinColumn(name = "board_id")
+    private Board board;
+
+    private String url;
+
 }
