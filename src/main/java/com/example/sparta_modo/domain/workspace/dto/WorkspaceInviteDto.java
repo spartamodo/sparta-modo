998502115fb802127d719ef461379f9b746b038f
dirtbagss <dirtbagag4@gmail.com>
2024-12-29 21:52:53 +0900

    add : 워크스페이스 멤버초대 Dto 생성

---
+++ src/main/java/com/example/sparta_modo/domain/workspace/dto/WorkspaceInviteDto.java
@@ -0,0 +1,33 @@
+package com.example.sparta_modo.domain.workspace.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Builder;
+import lombok.Getter;
+
+public class WorkspaceInviteDto {
+
+    @Getter
+    public static class Request {
+
+        @NotBlank(message = "email 입력은 필수입니다")
+        @Email(message = "올바른 email 형식이 아닙니다")
+        private String email;
+
+    }
+
+    @Getter
+    public static class Response {
+        private Long workspaceId;
+
+        private Long userId;
+
+        private String email;
+
+        public Response(Long workspaceId, Long userId, String email) {
+            this.workspaceId = workspaceId;
+            this.userId = userId;
+            this.email = email;
+        }
+    }
+}
