15c50dac8d360acc4dc1e9964e82794eb0abe85f
dirtbagss <dirtbagag4@gmail.com>
2024-12-30 20:53:27 +0900

    update : workspace 예외처리 추가

--- src/main/java/com/example/sparta_modo/global/exception/errorcode/WorkspaceErrorCode.java
+++ src/main/java/com/example/sparta_modo/global/exception/errorcode/WorkspaceErrorCode.java
@@ -8,7 +8,8 @@ import org.springframework.http.HttpStatus;
 @AllArgsConstructor
 public enum WorkspaceErrorCode {
 
-    ALREADY_DECIDED_ACCESS(HttpStatus.BAD_REQUEST, "이미 처리된 요청입니다");
+    ALREADY_DECIDED_ACCESS(HttpStatus.BAD_REQUEST, "이미 처리된 요청입니다"),
+    IMPOSSIBLE_MODIFY(HttpStatus.BAD_REQUEST, "ADMIN 으로 는 변경 할 수 없습니다.");
 
     private final HttpStatus httpStatus;
     private final String detail;
